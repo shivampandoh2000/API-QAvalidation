a292db98ab376c0d41d2560fae0201f50d2cd393
Shivam Pandoh <shivampandoh2000@gmail.com>
2022-05-27 14:58:50 +0530

    implement test listner

--- src/test/java/Apitesting/sampleproject/TestCases.java
+++ src/test/java/Apitesting/sampleproject/TestCases.java
@@ -29,9 +29,9 @@ public class TestCases {
 	}
 	
 //	GET
-	@Test
+	@Test(description="Fetching all user details.", testName="testFetchUsers")
 	public void testFetchUsers(){
-		ExtentReportManager.test = ExtentReportManager.reports.startTest("testFetchUsers", "Fetching all user details.");
+		ExtentReportManager.test = ExtentReportManager.getTest();
 		ExtentReportManager.test.log(LogStatus.INFO, "Specifying the base URI", "https://reqres.in");
 		ExtentReportManager.test.log(LogStatus.INFO, "API Call", "GET");
 		ExtentReportManager.test.log(LogStatus.INFO, "Resource url", "api/users?page=2");
@@ -43,9 +43,10 @@ public class TestCases {
 	
 //	GET
 	@SuppressWarnings("unchecked")
-	@Test
+	@Test(description="Fetching single user details.", testName="testFetchUser")
 	public void testFetchUser(){
-		ExtentReportManager.test = ExtentReportManager.reports.startTest("testFetchUser", "Fetching single user details.");
+		ExtentReportManager.test = ExtentReportManager.getTest();
+//		ExtentReportManager.test = ExtentReportManager.reports.startTest("testFetchUser", "Fetching single user details.");
 		ExtentReportManager.test.log(LogStatus.INFO, "Specifying the base URI", "https://reqres.in");
 		ExtentReportManager.test.log(LogStatus.INFO, "API Call", "GET");
 		ExtentReportManager.test.log(LogStatus.INFO, "Resource url", "api/users/2");
@@ -62,9 +63,9 @@ public class TestCases {
 	}
 	
 //	GET
-	@Test
+	@Test(description="User not found.", testName="testUserNotFound")
 	public void testUserNotFound(){
-		ExtentReportManager.test = ExtentReportManager.reports.startTest("testUserNotFound", "User not found.");
+		ExtentReportManager.test = ExtentReportManager.getTest();
 		ExtentReportManager.test.log(LogStatus.INFO, "Specifying the base URI", "https://reqres.in");
 		ExtentReportManager.test.log(LogStatus.INFO, "API Call", "GET");
 		ExtentReportManager.test.log(LogStatus.INFO, "Resource url", "api/users/23");
@@ -80,9 +81,9 @@ public class TestCases {
 	}
 	
 //	GET
-	@Test
+	@Test(description="Fetch list of resouces.", testName="testFetchListResource")
 	public void testFetchListResource(){
-		ExtentReportManager.test = ExtentReportManager.reports.startTest("testFetchListResource", "Fetch list of resouces.");
+		ExtentReportManager.test = ExtentReportManager.getTest();
 		ExtentReportManager.test.log(LogStatus.INFO, "Specifying the base URI", "https://reqres.in");
 		ExtentReportManager.test.log(LogStatus.INFO, "API Call", "GET");
 		ExtentReportManager.test.log(LogStatus.INFO, "Resource url", "api/unknown");
@@ -93,9 +94,9 @@ public class TestCases {
 	}
 	
 //	GET
-	@Test
+	@Test(description="Fetch single resouce.", testName="testFetchResource")
 	public void testFetchResource(){
-		ExtentReportManager.test = ExtentReportManager.reports.startTest("testFetchResource", "Fetch single resouce.");
+		ExtentReportManager.test = ExtentReportManager.getTest();
 		ExtentReportManager.test.log(LogStatus.INFO, "Specifying the base URI", "https://reqres.in");
 		ExtentReportManager.test.log(LogStatus.INFO, "API Call", "GET");
 		ExtentReportManager.test.log(LogStatus.INFO, "Resource url", "api/unknown/2");
@@ -106,9 +107,9 @@ public class TestCases {
 	}
 	
 //	GET
-	@Test
+	@Test(description="Resource not found.", testName="testFetchResourceNotFound")
 	public void testFetchResourceNotFound(){
-		ExtentReportManager.test = ExtentReportManager.reports.startTest("testFetchResourceNotFound", "Resource not found.");
+		ExtentReportManager.test = ExtentReportManager.getTest();
 		ExtentReportManager.test.log(LogStatus.INFO, "Specifying the base URI", "https://reqres.in");
 		ExtentReportManager.test.log(LogStatus.INFO, "API Call", "GET");
 		ExtentReportManager.test.log(LogStatus.INFO, "Resource url", "api/unknown/23");
@@ -125,9 +126,9 @@ public class TestCases {
 	
 	
 //	GET
-	@Test
+	@Test(description="Fetch Delayed Response.", testName="testDelayedResponse")
 	public void testDelayedResponse(){
-		ExtentReportManager.test = ExtentReportManager.reports.startTest("testDelayedResponse", "Fetch Delayed Response");
+		ExtentReportManager.test = ExtentReportManager.getTest();
 		ExtentReportManager.test.log(LogStatus.INFO, "Specifying the base URI", "https://reqres.in");
 		ExtentReportManager.test.log(LogStatus.INFO, "API Call", "GET");
 		ExtentReportManager.test.log(LogStatus.INFO, "Resource url", "api/users?delay=3");
@@ -139,9 +140,9 @@ public class TestCases {
 	
 	
 //	POST
-	@Test
+	@Test(description="Create a new User", testName="testCreateUser")
 	public void testCreateUser(){
-		ExtentReportManager.test = ExtentReportManager.reports.startTest("testCreateUser", "Create a new User");
+		ExtentReportManager.test = ExtentReportManager.getTest();
 		ExtentReportManager.test.log(LogStatus.INFO, "Specifying the base URI", "https://reqres.in");
 		ExtentReportManager.test.log(LogStatus.INFO, "API Call", "POST");
 		ExtentReportManager.test.log(LogStatus.INFO, "Payload", "{name: \"Shivam\", job: \"QA\"}");
@@ -157,9 +158,9 @@ public class TestCases {
 	
 	
 //	POST
-	@Test
+	@Test(description="User register successful", testName="testRegisterUserSuccessful")
 	public void testRegisterUserSuccessful(){
-		ExtentReportManager.test = ExtentReportManager.reports.startTest("testRegisterUserSuccessful", "User register successful");
+		ExtentReportManager.test = ExtentReportManager.getTest();
 		ExtentReportManager.test.log(LogStatus.INFO, "Specifying the base URI", "https://reqres.in");
 		ExtentReportManager.test.log(LogStatus.INFO, "API Call", "POST");
 		ExtentReportManager.test.log(LogStatus.INFO, "Payload", "{email: \"eve.holt@reqres.in\", password: \"123456\"}");
@@ -175,9 +176,9 @@ public class TestCases {
 	
 	
 //	POST
-	@Test
+	@Test(description="User register unsuccessful", testName="testRegisterUserUnsuccessful")
 	public void testRegisterUserUnsuccessful(){
-		ExtentReportManager.test = ExtentReportManager.reports.startTest("testRegisterUserUnsuccessful", "User register unsuccessful");
+		ExtentReportManager.test = ExtentReportManager.getTest();
 		ExtentReportManager.test.log(LogStatus.INFO, "Specifying the base URI", "https://reqres.in");
 		ExtentReportManager.test.log(LogStatus.INFO, "API Call", "POST");
 		ExtentReportManager.test.log(LogStatus.INFO, "Payload", "{email: \"eve.holt@reqres.in\"}");
@@ -192,9 +193,9 @@ public class TestCases {
 	
 	
 //	POST
-	@Test
+	@Test(description="User login successful", testName="testLoginSuccessful")
 	public void testLoginSuccessful(){
-		ExtentReportManager.test = ExtentReportManager.reports.startTest("testLoginSuccessful", "User login successful");
+		ExtentReportManager.test = ExtentReportManager.getTest();
 		ExtentReportManager.test.log(LogStatus.INFO, "Specifying the base URI", "https://reqres.in");
 		ExtentReportManager.test.log(LogStatus.INFO, "API Call", "POST");
 		ExtentReportManager.test.log(LogStatus.INFO, "Payload", "{email: \"eve.holt@reqres.in\", password: \"123456\"}");
@@ -210,9 +211,9 @@ public class TestCases {
 	
 	
 //	POST
-	@Test
+	@Test(description="User login unsuccessful", testName="testLoginUnSuccessful")
 	public void testLoginUnSuccessful(){
-		ExtentReportManager.test = ExtentReportManager.reports.startTest("testLoginUnSuccessful", "User login unsuccessful");
+		ExtentReportManager.test = ExtentReportManager.getTest();
 		ExtentReportManager.test.log(LogStatus.INFO, "Specifying the base URI", "https://reqres.in");
 		ExtentReportManager.test.log(LogStatus.INFO, "API Call", "POST");
 		ExtentReportManager.test.log(LogStatus.INFO, "Payload", "{email: \"eve.holt@reqres.in\"}");
